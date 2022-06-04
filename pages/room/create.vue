6971367ae8faf601f3928da96ed62b9d02747a9e
Mayo Sumi <>
2022-06-04 14:01:34 +0900

    feature: 超簡易的ルーム作成画面

--- pages/room/create.vue
+++ pages/room/create.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="create-room-container">
     <h1>CREATE ROOM</h1>
+    <form>
+      <input type="text" v-model="name" />
+      <input type="text" v-model="day" />
+      <button @click="createRoom">ROOMを作成</button>
+    </form>
   </div>
 </template>
 
 <script>
-import { defineComponent } from "@nuxtjs/composition-api";
+import { defineComponent, ref } from "@nuxtjs/composition-api";
 export default defineComponent({
   name: "CreateRoomPage",
+  setup() {
+    const name = ref(null);
+    const day = ref(null);
+    const createRoom = () => {
+      // TODO 受け取ったパラメータをfirebase storeに保存する
+      console.log(name.value, day.value);
+    };
+    return {
+      createRoom,
+      name,
+      day,
+    };
+  },
 });
 </script>
 
