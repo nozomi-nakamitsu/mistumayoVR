da348c5d9eff980e5d3ca4f92eb3570a8e370203
Mayo Sumi <>
2022-06-04 13:38:41 +0900

    feature: ログイン後のページ遷移

---
+++ config/routes.ts
@@ -0,0 +1,9 @@
+export const APP_ROUTES = {
+  login: { path: "/login", name: "ログイン" },
+};
+
+const roomPrefix = "/room";
+
+const ROOM_ROUTES = {
+  create: { path: `${roomPrefix}/create`, name: "ルーム作成" },
+};
---
+++ pages/room/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="create-room-container">
+    <h1>CREATE ROOM</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@nuxtjs/composition-api";
+export default defineComponent({
+  name: "CreateRoomPage",
+});
+</script>
+
+<style lang="scss">
+.create-room-container {
+  background: #fafafa;
+  margin: 15em auto;
+  padding: 4em;
+  max-width: 370px;
+}
+</style>
